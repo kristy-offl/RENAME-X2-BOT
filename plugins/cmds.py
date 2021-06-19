98d98187717d83e205e86c2ddc26b165cbc6179a
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-06-19 10:47:02 +0530

    Update cmds.py

--- plugins/cmds.py
+++ plugins/cmds.py
@@ -33,36 +33,3 @@ async def start(bot, update):
           parse_mode="html",
           disable_web_page_preview=True,
  
-
-@Clinton.on_message(filters.command(["help"]))
-async def helpme(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.HELP_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-
-@Clinton.on_message(filters.command(["about"]))
-async def abot(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.ABOUT_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-
-
-@Clinton.on_message(filters.command(["upgrade"]))
-async def upgra(bot, update):
-          await bot.send_message(
-          chat_id=update.chat.id,
-          text=Scripted.UPGRADE_TEXT,
-          parse_mode="html",
-          disable_web_page_preview=True,
-
-
-
-@Clinton.on_callback_query()
-async def button(bot, update):
- 
-      if  'DM'  in update.data:
-                await update.message.delete()
