a1bb30a87dda71cbf534c246b1106c576e7ef0b9
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2021-06-19 17:54:48 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -1,95 +1,135 @@
-#https://github.com/Clinton-Abraham/RENAMER-BOT
-
-import os
-import time
 import logging
-import pyrogram
-import mtb
-
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-if bool(os.environ.get("WEBHOOK", False)):
+import os
+import time
+import asyncio
+import pyrogram
 
+if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
 
-from PIL import Image
-from pyrogram import filters
-from mtb import mtb
-from database.database import *
-from pyrogram import Client as Clinton
-from hachoir.parser import createParser
+from script import script
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+
+from plugins.helpers import progress_for_pyrogram
+
 from hachoir.metadata import extractMetadata
-from functions.display_progress import progress_for_pyrogram
-from pyrogram.errors import UserNotParticipant, UserBannedInChannel
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from hachoir.parser import createParser
+
+from PIL import Image
+from database.database import *
 
 
+async def force_name(bot, message):
 
-@Clinton.on_message(filters.command(["rename"]))
-async def rename_doc(bot, update):
+    await bot.send_message(
+        message.reply_to_message.from_user.id,
+        "Enter new name for media\n\nNote : Extension not required",
+        reply_to_message_id=message.reply_to_message.message_id,
+        reply_markup=ForceReply(True)
+    )
 
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text(mtb.ACCESS_DENIED)
-               return
-        except UserNotParticipant:
-            await update.reply_text(text=mtb.JOIN_NOW_TEXT,
-                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton(text="ᴊᴏɪɴ ɴᴏᴡ 🔓", url=f"https://t.me/{Config.UPDATE_CHANNEL}") ]
-                ] 
-              )
-            )
-            return
-        except Exception:
-            await update.reply_text(mtb.CONTACT_MY_DEVELOPER)
-            return
 
-    if (" " in update.text) and (update.reply_to_message is not None):
-        cmd, file_name = update.text.split(" ", 1)
-        new_file = file_name[:60] + file_name[-4:]
-        description = mtb.CUSTOM_CAPTION.format(file_name)
+@Client.on_message(filters.private & filters.reply & filters.text)
+async def cus_name(bot, message):
+    
+    if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
+        asyncio.create_task(rename_doc(bot, message))     
+    else:
+        print('No media present')
+
+    
+async def rename_doc(bot, message):
+    
+    mssg = await bot.get_messages(
+        message.chat.id,
+        message.reply_to_message.message_id
+    )    
+    
+    media = mssg.reply_to_message
+
+    
+    if media.empty:
+        await message.reply_text('Why did you delete that 😕', True)
+        return
+        
+    filetype = media.document or media.video or media.audio or media.voice or media.video_note
+    try:
+        actualname = filetype.file_name
+        splitit = actualname.split(".")
+        extension = (splitit[-1])
+    except:
+        extension = "mkv"
+
+    await bot.delete_messages(
+        chat_id=message.chat.id,
+        message_ids=message.reply_to_message.message_id,
+        revoke=True
+    )
+    
+    if message.from_user.id not in Config.BANNED_USERS:
+        file_name = message.text
+        description = script.CUSTOM_CAPTION_UL_FILE.format(newname=file_name)
         download_location = Config.DOWNLOAD_LOCATION + "/"
-        c = await bot.send_message(
-            chat_id=update.chat.id,
-            text=mtb.TRYING_TO_DOWNLOAD,
-            reply_to_message_id=update.message_id
+
+        sendmsg = await bot.send_message(
+            chat_id=message.chat.id,
+            text=script.DOWNLOAD_START,
+            reply_to_message_id=message.message_id
         )
+        
         c_time = time.time()
         the_real_download_location = await bot.download_media(
-            message=update.reply_to_message,
+            message=media,
             file_name=download_location,
             progress=progress_for_pyrogram,
-            progress_args=(mtb.DOWNLOAD_START, c, c_time) )
-
+            progress_args=(
+                script.DOWNLOAD_START,
+                sendmsg,
+                c_time
+            )
+        )
         if the_real_download_location is not None:
             try:
                 await bot.edit_message_text(
-                    text=mtb.TRYING_TO_UPLOAD,
-                    chat_id=update.chat.id,
-                    message_id=c.message_id
+                    text=script.SAVED_RECVD_DOC_FILE,
+                    chat_id=message.chat.id,
+                    message_id=sendmsg.message_id
                 )
             except:
-                pass
-            new_file_name = download_location + file_name
+                await sendmsg.delete()
+                sendmsg = await message.reply_text(script.SAVED_RECVD_DOC_FILE, quote=True)
+
+            new_file_name = download_location + file_name + "." + extension
             os.rename(the_real_download_location, new_file_name)
-            logger.info(the_real_download_location)
-            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+            try:
+                await bot.edit_message_text(
+                    text=script.UPLOAD_START,
+                    chat_id=message.chat.id,
+                    message_id=sendmsg.message_id
+                    )
+            except:
+                await sendmsg.delete()
+                sendmsg = await message.reply_text(script.UPLOAD_START, quote=True)
+            # logger.info(the_real_download_location)
+
+            thumb_image_path = download_location + str(message.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
-                mes = await sthumb(update.from_user.id)
+                mes = await thumb(message.from_user.id)
                 if mes != None:
-                    m = await bot.get_messages(update.chat.id, mes.msg_id)
+                    m = await bot.get_messages(message.chat.id, mes.msg_id)
                     await m.download(file_name=thumb_image_path)
                     thumb_image_path = thumb_image_path
                 else:
-                    thumb_image_path = None
+                    thumb_image_path = None                    
             else:
                 width = 0
                 height = 0
@@ -102,29 +142,45 @@ async def rename_doc(bot, update):
                 img = Image.open(thumb_image_path)
                 img.resize((320, height))
                 img.save(thumb_image_path, "JPEG")
+
             c_time = time.time()
             await bot.send_document(
-            chat_id=update.chat.id,
-            document=new_file_name,
-            thumb=thumb_image_path,
-            caption=description,
-            reply_to_message_id=update.reply_to_message.message_id,
-            progress=progress_for_pyrogram,
-            progress_args=(mtb.UPLOAD_START, c, c_time))
-
-            try:
-                os.remove(the_real_download_location)
-                os.remove(thumb_image_path)
-            except:
-                pass
-            await bot.edit_message_text(
-                  text=mtb.UPLOAD_SUCCESS,
-                  chat_id=update.chat.id,
-                  message_id=c.message_id
+                chat_id=message.chat.id,
+                document=new_file_name,
+                thumb=thumb_image_path,
+                caption=description,
+                # reply_markup=reply_markup,
+                reply_to_message_id=message.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    script.UPLOAD_START,
+                    sendmsg, 
+                    c_time
+                )
             )
 
+            try:
+                os.remove(new_file_name)
+            except:
+                pass                 
+            try:
+                os.remove(thumb_image_path)
+            except:
+                pass  
+            try:
+                await bot.edit_message_text(
+                    text=script.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                    chat_id=message.chat.id,
+                    message_id=sendmsg.message_id,
+                    disable_web_page_preview=True
+                )
+            except:
+                await sendmsg.delete()
+                await message.reply_text(script.AFTER_SUCCESSFUL_UPLOAD_MSG, quote=True)
+                
     else:
         await bot.send_message(
-            chat_id=update.chat.id,
-            text=mtb.REPLY_TO_FILE,
-            reply_to_message_id=update.message_id)
+            chat_id=message.chat.id,
+            text="You're B A N N E D",
+            reply_to_message_id=message.message_id
+        )
